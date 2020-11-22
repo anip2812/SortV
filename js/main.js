553a97312fa9958b7f0db4ca19026034823e2bba
anip2812 <74119863+anip2812@users.noreply.github.com>
2020-11-22 08:20:41 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+let values = [];
+let w = 10;
+let states = [];
+let sleepDelay = 10;
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight-100);
+  values = new Array(floor(width / w));
+  for (let i = 0; i < values.length; i++) {
+       values[i] = random(height-100) + 100;
+       states[i] = -1;
+       }
+  frameRate(30);  
+}
+
+function draw() {
+  background(50);
+  
+  for (let i = 0; i < values.length; i++){
+    stroke(0);
+    fill(255);
+    if (states[i] == 0) { //seagreen
+      fill(46,139,87);
+    } else if (states[i] == 1) { //darkseagreen
+      fill(143,188,143);
+    } else if (states[i] == 2) { //grey
+      fill(160,160,160);
+    } else if (states[i] == 3) { //lightgrey
+      fill(200,200,200);
+    } else { 
+      fill(255);
+    }
+
+
+
+    rect(i * w, height - values[i], w, values[i]);
+  }   
+}
+
+function changeWidth(range) {
+   w = 110 - range;
+   setup();
+}
+
+
+function changeSpeed(range) {   
+   sleepDelay = (100-range)*10;   
+}
+
+
+
+
+  
+
+
+
+
+
+ 
+async function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+  //await sleep(sleepDelay);
+  
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
---
+++ js/sort.js
@@ -0,0 +1,169 @@
+async function quickSort(arr, start, end) {
+  if (start >= end) {
+    return;    
+  }  
+  let index = await quickSortPartition(arr, start, end);
+  states[index] = -1;
+ 
+  await Promise.all([
+    quickSort(arr, start, index - 1)    
+  ]);
+  
+  for (let i = start; i <= index; i++) {
+    states[i] = -1;
+  }
+  
+  await Promise.all([    
+    quickSort(arr, index + 1, end)
+  ]);  
+
+  states[end] = -1;
+}
+
+async function quickSortPartition(arr, start, end) {
+  states[end] = 0;
+  for (let i = start; i < end; i++) {
+    states[i] = 1;
+  }
+  
+  let pivotIndex = start;
+  let pivotValue = arr[end];
+  
+  for (let i = start; i < end; i++) {
+    
+    states[i] = 0;
+    await sleep(sleepDelay);
+    states[i] = 2;
+
+    if (arr[i] < pivotValue) {
+      await swap(arr, i, pivotIndex);
+      states[pivotIndex] = 3;
+      pivotIndex++;    
+    }    
+  }
+  // states[start] = 2;  
+  states[end] = 2;
+  await  swap(arr, pivotIndex, end);
+  return pivotIndex;
+}
+
+async function mergeSort(arr, start, end) {
+  
+  if (start >= end) {
+    return;    
+  }
+    
+  let mid = Math.floor(start+((end-start) / 2));
+
+  await Promise.all([
+    mergeSort(arr, start, mid),
+    // mergeSort(arr, mid + 1, end)
+  ]);await Promise.all([
+    // mergeSort(arr, start, mid),
+    mergeSort(arr, mid + 1, end)
+  ]);
+
+  let left = start;
+  let right = mid + 1;
+
+  for (let i = start; i <= end; i++) {
+    states[i] = 1;
+  }
+
+  while (left <= mid && right <= end) {
+    states[left] = 0;
+    states[right] = 0;
+    await sleep(sleepDelay);
+    if (arr[right] <= arr[left]) {
+        states[left] = -1;
+        states[right] = 1;            
+        let temp = arr[right];
+        arr.splice(right, 1);
+        arr.splice(left, 0, temp);
+        left ++;        
+        right ++;
+        mid ++;      
+    }
+    else {
+      states[left] = -1;
+      states[right] = 1;
+      left ++;      
+    }        
+  }
+
+  for (let i = left; i <= end; i++) {
+      states[i] = -1;
+    }
+}
+
+async function bubbleSort(arr) {
+  for (let i = 0; i < values.length; i++){
+    states[i] = 1;
+  }
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length-i-1; j++){        
+      states[j] = 0;
+      states[j+1] = 0;
+      await sleep(sleepDelay);
+      if (arr[j] > arr[j+1]) {       
+        swap(arr, j, j+1);        
+      }
+      states[j] = 1;
+      states[j+1] = -1;
+    }
+  }
+  states[j] = -1;
+}
+
+async function insertionSort(arr) {
+
+  for (let i = 0; i < values.length; i++){
+    states[i] = 1;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let j = i - 1;
+    while (j >= 0) {
+      states[j] = 0;
+      states[j+1] = 0;
+      await sleep(sleepDelay);
+      states[j] = -1;
+      states[j+1] = -1; 
+      if (arr[j] > arr[j+1]) {   
+        swap(arr, j, j+1);
+        j--;
+      } else {
+        j = -1;
+      }                
+    }
+  }
+}
+
+async function selectionSort(arr) {
+
+  for (let i = 0; i < values.length; i++){
+    states[i] = 1;
+  }
+
+  for (let i = 0; i < arr.length; i++) {        
+    let min = i;
+    states[min] = 0;
+    for (let j = i+1; j < arr.length; j++) {
+      states[j] = 0;
+      await sleep(sleepDelay);
+      if(arr[j] < arr[min]){
+        states[min] = 1;
+        min = j; 
+        states[min] = 0;
+      }else{
+        states[j] = 1;  
+      }
+      
+    }
+    if (min != i) {
+      swap(arr, i, min);
+    }
+    states[i] = -1;    
+    states[min] = 1;  
+  }
+}
